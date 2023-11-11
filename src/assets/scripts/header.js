49d0b8fb4089c44c7b89e90910970dfaac0cc222
unknown <ryo.bang@namtech.sg>
2023-11-11 23:30:29 +0700

    done header desktop

---
+++ src/assets/scripts/header.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // open sub menu
+    function onpenSubmenu(){
+        $("#header .menu .menu-item.has-children > .sub-menu .menu-item a").on("click", function(e){
+            e.preventDefault();
+            $(this).addClass("active");
+            $(this).closest(".menu-item").siblings().children(".menu-link").removeClass("active");
+        })
+    }
+    onpenSubmenu();
+})
