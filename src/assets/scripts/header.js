602736e04c09dd951596f4e0e37c6d99ee472890
unknown <ryo.bang@namtech.sg>
2023-11-12 12:56:46 +0700

    done section top destination

--- src/assets/scripts/header.js
+++ src/assets/scripts/header.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     // open sub menu
-    function onpenSubmenu(){
+    function openSubmenu(){
         $("#header .menu .menu-item.has-children > .sub-menu .menu-item a").on("click", function(e){
             e.preventDefault();
             $(this).addClass("active");
             $(this).closest(".menu-item").siblings().children(".menu-link").removeClass("active");
         })
     }
-    onpenSubmenu();
+    openSubmenu();
 })
